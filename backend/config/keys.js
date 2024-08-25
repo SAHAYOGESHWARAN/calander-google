225f016e715ea514b475b169b1089ab0d7927dc2
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-25 10:46:47 +0530

    CG

---
+++ backend/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: 'YOUR_GOOGLE_CLIENT_ID',
+    googleClientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+    mongoURI: 'YOUR_MONGODB_URI',
+    emailServiceAPI: 'YOUR_EMAIL_SERVICE_API_KEY',
+    smsServiceAPI: 'YOUR_SMS_SERVICE_API_KEY'
+};
