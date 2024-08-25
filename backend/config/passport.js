f80dc4d4f4917501596943ed25317f861c4f5f56
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-25 10:28:40 +0530

    CG

---
+++ backend/config/passport.js
@@ -0,0 +1,44 @@
+// /backend/config/passport.js
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback',
+},
+    async (accessToken, refreshToken, profile, done) => {
+        const { id, displayName, emails } = profile;
+        try {
+            let user = await User.findOne({ googleId: id });
+            if (!user) {
+                user = new User({
+                    googleId: id,
+                    name: displayName,
+                    email: emails[0].value,
+                });
+                await user.save();
+            }
+            done(null, user);
+        } catch (err) {
+            done(err, null);
+        }
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
+});
